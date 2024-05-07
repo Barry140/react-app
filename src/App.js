665421f39442b20d94c0ba0418f2c12776dfed28
Barry140 <162043824+Barry140@users.noreply.github.com>
2024-05-07 12:29:57 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [people, setPeople] = useState([
+    {
+      name:"john",
+      age: 10,
+      address:"TD"
+    },
+    {
+      name:"johnson",
+      age: 10,
+      address:"TD"
+    },
+    {
+      name:"The rock",
+      age: 20,
+      address:"TD"
+    },
+    {
+      name:"Dwayne ",
+      age: 10,
+      address:"TD"
+    }
+  ]
+)
+  
+  const handleItem = (e) => {
+    e.preventDefault();
+    // Get values of new people from form (name, age, address)
+    const newName = e.target.name.value;
+    const newAge = e.target.age.value;
+    const newAddress = e.target.address.value;
+    // Add new people to array of people
+    setPeople(prevState => [ ...prevState, {name:newName,age:newAge,address:newAddress} ] )
+
+    // Reset form values after adding new people
+    e.target.reset();
+  }
+
+  const listitem = people.map((item, idx) => <tr key={idx}>
+    <td>{item.name}</td>
+    <td>{item.age}</td>
+    <td>{item.address}</td>
+  </tr>)
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <table>
+      <tr>
+        <th>Name</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      {listitem}
+    </table>
+
+    <form onSubmit={handleItem}>
+      <input type="text" name="name"/>
+      <input type="number" name="age"/>
+      <input type="text" name="address"/>
+      <button type="submit">ADD PEOPLE</button>
+    </form>
+
       </header>
     </div>
   );
