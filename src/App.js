7bb11162dd9f362377b08723dd393212d176cf76
Barry140 <162043824+Barry140@users.noreply.github.com>
2024-06-24 12:53:34 +0700

    git push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import axios from 'axios';
+
+
 
 const defaultFormData = {
   name: '',
@@ -22,7 +28,20 @@ function App() {
     index: null
   });
 
-  
+  const [searchTask, setSearchTask] = useState('')
+  const handleOnSearchChange = (e) => {
+      setSearchTask(e.target.value);
+  }
+  const handleSearch = () => {
+    if(searchTask){
+      setPeople(prevState => {
+        return prevState.filter ((p) => {
+          return p.name === searchTask;
+        })
+      })
+    }
+    else updateList();
+  }
 
   const [people, setPeople] = useState([])
 
@@ -50,13 +69,15 @@ function App() {
     }))
     return Object.keys(formErrors).length === 0 && formErrors.constructor === Object;
   }
-  const handleItem = (e) => {
+
+  const handleItem = async (e) => {
     e.preventDefault(); 
     // run when there are values in form
     if (validateValues()) {
       const { index } = editPeople
       if (index !== null) {
-        setPeople(prevState => {
+          await patchItem(index);
+          setPeople(prevState => {
           return prevState.map((p, idx) => {
             if (idx === index) {
               const a = {
@@ -74,6 +95,7 @@ function App() {
           name:peopleFormData.name,
           status:peopleFormData.status
         } ] )
+        postList();
       }
       console.log('testt');
       // Reset form values after adding new people
@@ -83,29 +105,22 @@ function App() {
         status: ''
       })
       setEditPeople({index: null});
-    }
-
+      }
+      updateList();
   }
-  const handleEdit = (idx) => {
-
-      const b = people[idx];
-      console.log(b, "b");
-      setPeopleFormData({...b});
+  const handleEdit = async (id) => {
+      await getItem(id) 
       setEditPeople(prevState => ({
         ...prevState,
-        index: idx
+        index: id
       }))
-      
   }
-  const handleDelete = (idx) => {
-      setPeople(prevState => {
-        return prevState.filter ((p,i) => {
-          return i !== idx
-        })
-      })
-    }
-
-    //
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:3001/list/${id}`)
+    updateList();
+  }
+  
+  //
   const [show, setShow] = useState(false);
   const handleCloseButton = () => {
     if(validateValues()){
@@ -119,94 +134,155 @@ function App() {
     return setShow(false)
   };
   const handleShow = () => setShow(true);
-  
-  const listitem = people.map((item, idx) => <tr key={idx}>
-    <td style={{ textAlign: 'center' }}>{idx}</td>
-    <td style={{ textAlign: 'center' }}>{item.name}</td>
-    <td style={{ textAlign: 'center' }}>
-      {item.status === 'ok' ? (
-          <p className="text-uppercase">👍 {item.status}</p>
-      ) : item.status === 'good' ? (
-        <p className="text-capitalize">✌️ {item.status}</p>
-      ) : (
-          <p>🎱 {item.status}</p>
-      )}
-    </td>
-    <td style={{ textAlign: 'center' }}>
-      {/* <Button variant="outline-dark" type="button" onClick={() => handleEdit(idx)}>✏</Button> */}
-      <Button variant="outline-dark" type="button" onClick={() => {handleShow(); handleEdit(idx)}}>✏</Button>
-    </td>
-    <td style={{ textAlign: 'center' }}><Button variant="outline-dark" type="button" onClick={() => handleDelete(idx)}>🗑</Button></td>
+  //
+
+  const getItemStatus = (item) => {
+    switch (item) {
+      case 'ok': {
+        return (
+          <p className="text-uppercase">👍 {item}</p>
+        ); 
+      }
+      case 'good': {
+        return (
+          <p className="text-uppercase">👍 {item}</p>
+        ); 
+      }
+      default:
+        return <p>🎱 {item}</p>;
+      }
+  } 
+  const listitem = people.map((item, idx) => 
+    <tr key={idx}>
+    <td className='text-center'>{idx + 1}</td>
+    <td className='text-center'>{item.name}</td>
+    <td style={{ textAlign: 'center' }}>{getItemStatus(item.status)}</td>
+    <td style={{ textAlign: 'center' }}><Button variant="outline-dark" type="button" onClick={() => {handleShow(); handleEdit(item.id)}}>✏</Button></td>
+    <td style={{ textAlign: 'center' }}><Button variant="outline-dark" type="button" onClick={() => handleDelete(item.id)}>🗑</Button></td>
   </tr>)
   
-  
-  
+  const updateList = async () => {
+    await axios.get('http://localhost:3001/list')
+    .then(response => {
+      setPeople(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  const getItem = async (id) => {
+    await axios.get(`http://localhost:3001/list/${id}`)
+    .then(response => {
+      return setPeopleFormData(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  const patchItem = async (idx) => {
+    await axios
+      .put(`http://localhost:3001/list/${idx}`, {
+        name: peopleFormData.name,
+        status: peopleFormData.status
+      })
+      .then(response => {
+        console.log("Item da thay doi:", response?.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+  const postList = async () => {
+      try {
+        const response = await axios.post('http://localhost:3001/list', {
+          ...peopleFormData
+        });
+        console.log(response);
+        console.log('Data posted successfully!');
+        // Optionally handle response from server
+        console.log('Server response:', response.data);
+      } catch (error) {
+        console.error('Error posting data:', error);
+        // Handle error scenario
+      }
+    } 
+  //Sử dụng axios để fetch dữ liệu
+  useEffect(() => {
+    updateList()
+  }, [])
 
   return (
     <>
     <Container>
-
     <div className="App">
-
       <header className="App-header">
         <h1>TODO list Demo App</h1>
         <p>Do it now</p>
+      </header>
 
-        <div style={{ textAlign: 'right' }}>
-          <Button  variant="outline-info" onClick={handleShow}>
-           ADD TASK
-          </Button>
-        </div>
 
-        <Modal show={show} onHide={handleClose}>
+      <div>
+        <Row>
+          <Col sm={4} className='d-flex '>
+            <input placeholder='Task name to search..' type="search" name="name" value={searchTask} onChange={handleOnSearchChange}/>
+          </Col>
+          <Col sm={1} className='d-flex p-0'>
+            <Button variant="outline-primary"  onClick={handleSearch}>SEARCH</Button>
+          </Col>
+          <Col sm={1} className='d-flex p-0'>
+            <Button variant="secondary" size="sm" onClick={updateList}>Show all</Button>
+          </Col>
+          <Col  style={{textAlign: 'right'}}>
+            <Button variant="outline-primary" onClick={handleShow}>ADD TASK</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Table striped  hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Task Name</th>
+                  <th>Task Status</th>
+                  <th>Edit</th>
+                  <th>Remove</th>
+                </tr>
+              </thead>
+              <tbody>
+                {listitem}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </div> 
+
+    </div>
+
+    <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{editPeople.index !== null ? 'EDIT TASK INFO' : 'ADD TASK INFO'}</Modal.Title>
         </Modal.Header>
         
         <Form onSubmit={handleItem}>
-        <Modal.Body>
-            {/* {JSON.stringify(errors)} */} 
-            <div style={{ width:'100%'}}>
-              <Form.Label >Task Name :</Form.Label>
-              <input type="text" name="name" value={peopleFormData.name} onChange={handleOnChange}/>
-              {errors.name && <div style={{color: "red", textAlign: "left"}}><small>{errors.name}</small></div>}
-              <br></br>
-              <Form.Label >Task status :</Form.Label>
-              <input type="text" name="status" value={peopleFormData.status} onChange={handleOnChange}/>
-              {errors.status && <div style={{color: "red", textAlign: "left"}}><small>{errors.status}</small></div>}
-              {/* <Button variant="outline-info" type="submit">{editPeople.index !== null ? 'EDIT' : 'ADD'} TASK</Button> */}
-            </div>
-        </Modal.Body>
-
-        <Modal.Footer>          
-          <Button variant="secondary" onClick={handleClose}>Close</Button>
-          <Button variant="primary" type="submit" onClick={handleCloseButton}>{editPeople.index !== null ? ('SAVE') : 'ADD'} </Button>
-        </Modal.Footer>
-
+          <Modal.Body>
+              {/* {JSON.stringify(errors)} */} 
+              <div style={{ width:'100%'}}>
+                <Form.Label >Task Name :</Form.Label>
+                <input type="text" name="name" value={peopleFormData.name} onChange={handleOnChange}/>
+                {errors.name && <div style={{color: "red", textAlign: "left"}}><small>{errors.name}</small></div>}
+                <br></br>
+                <Form.Label >Task status :</Form.Label>
+                <input type="text" name="status" value={peopleFormData.status} onChange={handleOnChange}/>
+                {errors.status && <div style={{color: "red", textAlign: "left"}}><small>{errors.status}</small></div>}
+              </div>
+          </Modal.Body>
+          <Modal.Footer>          
+            <Button variant="secondary" onClick={handleClose}>Close</Button>
+            <Button variant="primary" type="submit" onClick={handleCloseButton}>{editPeople.index !== null ? ('SAVE') : 'ADD'} </Button>
+          </Modal.Footer>
         </Form>
-
-      </Modal>  
-
-        <Table striped  hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Task Name</th>
-              <th>Task Status</th>
-              <th>Edit</th>
-              <th>Remove</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {listitem}
-          </tbody>
-        </Table>
-
-      </header>
-
-    </div>
-
+      </Modal>
     </Container>
     </>
   );
